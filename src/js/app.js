b7986ff3e745c2935c9ad6f9486249b7a8d5ee92
stasumanskiy <stasumanskiy@gmail.com>
2018-05-17 18:39:15 +0300

    add map

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,2 @@
-import sayHello from './lib/sayHello.js';
+import './components';
 
-sayHello();
---
+++ src/js/components/map.js
@@ -0,0 +1,213 @@
+export default class Map {
+  constructor(config) {
+    this._mapHTML = config.map.get(0);
+    this._icon = './img/marker.png';
+    this._center = {
+      lat: +config.map.data('center-lat'),
+      lng: +config.map.data('center-lng')
+    };
+    this.modal = null,
+    this.duration = 300;
+    this._init();
+  }
+  _init() {
+    this._createMap();
+  }
+  _createMap() {
+    this._map = new google.maps.Map(this._mapHTML, {
+      center: this._center,
+      zoom: 14,
+      disableDefaultUI: true,
+      scrollwheel: false,
+      zoomControl: true,
+      backgroundColor: '#e9e9e9',
+      styles: [
+        {
+          'featureType': 'all',
+          'elementType': 'labels.text.fill',
+          'stylers': [
+            {
+              'saturation': 36
+            },
+            {
+              'color': '#000000'
+            },
+            {
+              'lightness': 40
+            }
+          ]
+        },
+        {
+          'featureType': 'all',
+          'elementType': 'labels.text.stroke',
+          'stylers': [
+            {
+              'visibility': 'on'
+            },
+            {
+              'color': '#000000'
+            },
+            {
+              'lightness': 16
+            }
+          ]
+        },
+        {
+          'featureType': 'all',
+          'elementType': 'labels.icon',
+          'stylers': [
+            {
+              'visibility': 'off'
+            }
+          ]
+        },
+        {
+          'featureType': 'administrative',
+          'elementType': 'geometry.fill',
+          'stylers': [
+            {
+              'color': '#000000'
+            },
+            {
+              'lightness': 20
+            }
+          ]
+        },
+        {
+          'featureType': 'administrative',
+          'elementType': 'geometry.stroke',
+          'stylers': [
+            {
+              'color': '#000000'
+            },
+            {
+              'lightness': 17
+            },
+            {
+              'weight': 1.2
+            }
+          ]
+        },
+        {
+          'featureType': 'landscape',
+          'elementType': 'geometry',
+          'stylers': [
+            {
+              'color': '#000000'
+            },
+            {
+              'lightness': 20
+            }
+          ]
+        },
+        {
+          'featureType': 'poi',
+          'elementType': 'geometry',
+          'stylers': [
+            {
+              'color': '#000000'
+            },
+            {
+              'lightness': 21
+            }
+          ]
+        },
+        {
+          'featureType': 'road.highway',
+          'elementType': 'geometry.fill',
+          'stylers': [
+            {
+              'color': '#000000'
+            },
+            {
+              'lightness': 17
+            }
+          ]
+        },
+        {
+          'featureType': 'road.highway',
+          'elementType': 'geometry.stroke',
+          'stylers': [
+            {
+              'color': '#000000'
+            },
+            {
+              'lightness': 29
+            },
+            {
+              'weight': 0.2
+            }
+          ]
+        },
+        {
+          'featureType': 'road.arterial',
+          'elementType': 'geometry',
+          'stylers': [
+            {
+              'color': '#000000'
+            },
+            {
+              'lightness': 18
+            }
+          ]
+        },
+        {
+          'featureType': 'road.local',
+          'elementType': 'geometry',
+          'stylers': [
+            {
+              'color': '#000000'
+            },
+            {
+              'lightness': 16
+            }
+          ]
+        },
+        {
+          'featureType': 'transit',
+          'elementType': 'geometry',
+          'stylers': [
+            {
+              'color': '#000000'
+            },
+            {
+              'lightness': 19
+            }
+          ]
+        },
+        {
+          'featureType': 'water',
+          'elementType': 'geometry',
+          'stylers': [
+            {
+              'color': '#000000'
+            },
+            {
+              'lightness': 17
+            }
+          ]
+        }
+      ]
+    });
+    var that = this;
+    var marker = new google.maps.Marker({
+      position: this._center,
+      map: null,
+      icon: {
+        url: that._icon
+      }
+    });
+
+    // To add the marker to the map, call setMap();
+    marker.setMap(this._map);
+  }
+};
+
+let map = $('.js-map');
+if (map.length) {
+  map.each(function() {
+    new Map({
+      map: $(this)
+    });
+  });
+};
